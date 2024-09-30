d132beca20f83132bdc1bb8cada841c9c9ece669
Khanomtom <kunakron007@gmail.com>
2024-09-30 22:24:14 +0700

    111

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -10,6 +10,9 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
   }
 }
